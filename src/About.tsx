511271d35a00aa32f8581893468cf24da0b555c8
German <germanolle@gmail.com>
2023-02-07 17:48:14 -0300

    Change about temp image

--- src/About.tsx
+++ src/About.tsx
@@ -44,7 +44,7 @@ const SKILLS: SkillProps[] = [
 function About() {
   return (
     <InlineContent>
-      <img src='http://miro.afracode.com/assets/images/about/5-1.jpg' />
+      <img width={400} height={'auto'} src='https://images.pexels.com/photos/5926370/pexels-photo-5926370.jpeg' />
       <div>
         <Dot text='About Me' />
         <Title>Hello, I am German Olle</Title>
