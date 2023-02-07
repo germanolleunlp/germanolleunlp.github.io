271d250202f5c08f8829a0c852d5d1576c900908
German <germanolle@gmail.com>
2023-02-06 21:54:38 -0300

    Add animation on route change

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,17 +1,46 @@
 import React from 'react';
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import styled from 'styled-components';
+import { motion } from "framer-motion";
 import BurgerMenu from "./BurgerMenu";
 import FollowMe from "./FollowMe";
 import NavigationDots from "./NavigationDots";
 
+const pageVariants = {
+  initial: {
+    opacity: 0
+  },
+  in: {
+    opacity: 1
+  },
+  out: {
+    opacity: 0
+  }
+};
+
+const pageTransition = {
+  type: "tween",
+  ease: "linear",
+  duration: 2
+};
+
 function Layout() {
+  const { pathname } = useLocation();
+
   return (
     <Center>
       <BurgerMenu />
       <FollowMe />
       <Content>
-        <Outlet />
+        <motion.div
+          key={pathname}
+          initial="initial"
+          animate="in"
+          variants={pageVariants}
+          transition={pageTransition}
+        >
+          <Outlet />
+        </motion.div>
       </Content>
       <NavigationDots />
     </Center>
